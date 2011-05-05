5072859959031c375cd53c56b78a50ad433ffdef
bartosz <bartosz@boxtale.com>
2011-05-05 09:29:40 +0000

    get order upse

--- samples/get_order.php
+++ samples/get_order.php
@@ -7,7 +7,7 @@ ob_start();
 header('Content-Type: text/html; charset=utf-8');
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 require_once('../utils/header.php');
-require_once $_SERVER['DOCUMENT_ROOT'].'/librairie/utils/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/api/librairie/utils/autoload.php';
 $orderPMStyle = 'style="font-weight:bold;"';
 // Informations sur l'expéditeur et le destinataire 
 $from = array("pays" => "FR", "code_postal" => "75002", "type" => "particulier",
@@ -22,21 +22,28 @@ $to = array("pays" => "FR", "code_postal" => "13005", "type" => "particulier",
 // Informations sur la cotation
 $quotInfo = array("collecte" => date("Y-m-d"), 
 "delai" => "aucun",  "code_contenu" => 10120,
-// "operateur" => "UPSE", //<= opérateur UPS,vous pouvez décommenter et commenter les lignes suivantes (jusqu'à retrait.pointrelais) pour voir ce que cela donne
-// "disponibilite.HDE" => "09:00", 
-// "disponibilite.HLE" => "19:00", 
-"operateur" => "SOGP",
-"collection_type" => "DROPOFF_POINT",
-"delivery_type" => "PICKUP_POINT",
-"depot.pointrelais" => "SOGP-C3051", 
-"retrait.pointrelais" => "SOGP-Q1117",
-"prix_max_ttc" => 20,
+ "operateur" => "UPSE", //<= opérateur UPS,vous pouvez décommenter et commenter les lignes suivantes (jusqu'à retrait.pointrelais) pour voir ce que cela donne
+ "service" => "Standard",
+ "disponibilite.HDE" => "09:00", 
+ "disponibilite.HLE" => "19:00", 
+ "assurance.selection" => true,
+ "assurance.emballage" => "Boîte",
+ "assurance.materiau" => "Film opaque",
+ "assurance.protection" => "Calage papier",
+ "assurance.fermeture" => "Agrafes",
+ "valeur" => 300,
+// "operateur" => "SOGP",
+// "collection_type" => "DROPOFF_POINT",
+// "delivery_type" => "PICKUP_POINT",
+// "depot.pointrelais" => "SOGP-C3051", 
+// "retrait.pointrelais" => "SOGP-Q1117",
+//"prix_max_ttc" => 20,
 "description" => "Le Monde, années 1990-1992"
 );
-$cotCl = new Env_Quotation(array("user" => "bbc", "pass" => "bbc", "key" => "bbc"));
+$cotCl = new Env_Quotation(array("user" => "bartosz", "pass" => "bartOOOSw", "key" => "xx00xxYY__AEZRS"));
 $cotCl->setPerson("expediteur", $from);
 $cotCl->setPerson("destinataire", $to);
-$cotCl->setType("colis", array("poids" => 2, "longueur" => 30, "largeur" => 44, "hauteur" => 44));
+$cotCl->setType("colis", array( "assurance.selection" => true,"poids" => 2, "longueur" => 13, "largeur" => 11, "hauteur" => 12));
 $orderPassed = $cotCl->makeOrder($quotInfo, true);
 if(!$cotCl->curlError && !$cotCl->respError) { 
   if($orderPassed) {
