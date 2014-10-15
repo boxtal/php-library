39597446ed5aca2be63759f140a44041c43991b4
thehuy <thehuy@boxtale.com>
2014-10-15 09:47:36 +0000

    #1881 : Replacing dev@envoimoinscher.com by informationAPI@envoimoinscher.com and getting comments back

--- env/WebService.php
+++ env/WebService.php
@@ -1,27 +1,12 @@
 <?php
-/**
- * 2007-2014 PrestaShop
+/** 
+ * EnvoiMoinsCher web service main class.
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- * @author    EnvoiMoinsCher <informationapi@boxtale.com>
- * @copyright 2007-2014 PrestaShop SA / 2011-2014 EnvoiMoinsCher
- * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- * International Registred Trademark & Property of PrestaShop SA
+ * The class handles request operations like : putting post and get parameters, calling 
+ * API and getting the result of this request.
+ * @package Env
+ * @author EnvoiMoinsCher <informationAPI@envoimoinscher.com>
+ * @version 1.0
  */
 
 define('ENV_TEST', 'test');
