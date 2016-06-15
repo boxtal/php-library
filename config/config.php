63ee988cc6c2574bcea77cd2bb2476f77bb71649
Ahmed BAHET <ahmed.bahet@boxtale.com>
2016-06-15 13:21:12 +0200

    Updated config file

--- config/config.php
+++ config/config.php
@@ -1,26 +1,31 @@
 <?php
-define("EMC_MODE", "test"); // change to 'prod' to test production server (this WILL make an order if you test makeOrder function !)
+define('EMC_MODE', 'test'); // change to 'prod' to test production server (this WILL make an order if you test makeOrder function !)
 
-if (EMC_MODE == "prod") {
-    define("EMC_USER", "abahet");
-    define("EMC_PASS", "boxtale");
-    define("EMC_KEY", "h2ucikm0");
+if (EMC_MODE == 'prod') {
+    define('EMC_USER', '');
+    define('EMC_PASS', '');
+    define('EMC_KEY', '');
 } else {
-    define("EMC_USER", "abahet");
-    define("EMC_PASS", "boxtale");
-    define("EMC_KEY", "h2ucikm0");
-    //define("EMC_KEY", "0mqfew54");
+    define('EMC_USER', '');
+    define('EMC_PASS', '');
+    define('EMC_KEY', '');
 }
 
-$uriExploded = explode("/", $_SERVER['REQUEST_URI']);
 
+
+$uriExploded = explode("/", $_SERVER['REQUEST_URI']);
 if (in_array("samples", $uriExploded) ||  in_array("test", $uriExploded)) {
     define("EMC_PARENT_DIR", "../");
 } else {
-   define("EMC_PARENT_DIR", "");
+    define("EMC_PARENT_DIR", "");
 }
 
 
+/**
+ * function to handle API errors
+ * @param  [type] $lib [description]
+ * @return [type]      [description]
+ */
 function handle_errors($lib)
 {
     if ($lib->resp_error) {
@@ -32,14 +37,3 @@ function handle_errors($lib)
         echo "Unable to send the request: ".$lib->curl_error_text;
     }
 }
-
-
-$week_days = array(
-    1 => 'Monday',
-    2 => 'Tuesday',
-    3 => 'Wednesday',
-    4 => 'Thursday',
-    5 => 'Friday',
-    6 => 'Saturday',
-    7 => 'Sunday'
-);
